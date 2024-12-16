b3a5c92d510e0040967cec1657cf8caee0952b8e
Martin Roehricht <martin.roehricht@hs-esslingen.de>
2024-12-16 10:56:19 +0100

    Added constness

--- src/OpenMeteoWeatherService.cpp
+++ src/OpenMeteoWeatherService.cpp
@@ -8,20 +8,20 @@ using json = nlohmann::json;
 WeatherData OpenMeteoWeatherService::getWeatherData(double latitude, double longitude) const {
     httplib::Client client("http://api.open-meteo.com");
     std::string path = "/v1/forecast?latitude=" + std::to_string(latitude)
-                       + "&longitude=" + std::to_string(longitude) + "&current_weather=true";
+        + "&longitude=" + std::to_string(longitude) + "&current_weather=true";
 
     auto res = client.Get(path.c_str());
 
     if (res && res->status == 200) {
         json response = json::parse(res->body, nullptr, false);
         if (!response.is_discarded() && response.contains("current_weather")) {
-            double temperature = response["current_weather"]["temperature"].get<double>();
-            double windSpeed = response["current_weather"]["windspeed"].get<double>();
-            return {temperature, windSpeed, true};
+            const double temperature = response["current_weather"]["temperature"].get<double>();
+            const double windSpeed = response["current_weather"]["windspeed"].get<double>();
+            return { temperature, windSpeed, true };
         }
     }
 
-    return {std::numeric_limits<double>::quiet_NaN(),
+    return { std::numeric_limits<double>::quiet_NaN(),
             std::numeric_limits<double>::quiet_NaN(),
-            false};
+            false };
 }
